ce1d4826e354cb7c22666dbf23fa909a5359ba03
Seneral <lev.gaeher@online.de>
2015-06-12 23:26:27 +0200

    Committed Update

--- Nodes/DisplayNode.cs
+++ Nodes/DisplayNode.cs
@@ -3,40 +3,45 @@ using UnityEditor;
 using System.Collections;
 
 [System.Serializable]
-public class DisplayNode : Node {
-    public bool assigned;
-    public float value;
+public class DisplayNode : Node 
+{
+	public bool assigned = false;
+	public float value = 0;
 
-    public static DisplayNode Create(Rect NodeRect) { // This function has to be registered in Node_Editor.ContextCallback
-        DisplayNode node = CreateInstance<DisplayNode>();
+	public static DisplayNode Create (Rect NodeRect) 
+	{ // This function has to be registered in Node_Editor.ContextCallback
+		DisplayNode node = CreateInstance <DisplayNode> ();
 
-        node.name = "Display Node";
-        node.rect = NodeRect;
+		node.name = "Display Node";
+		node.rect = NodeRect;
+		
+		NodeInput.Create (node, "Value", TypeOf.Float);
 
-        NodeInput.Create(node, "Value", typeof(float));
+		node.Init ();
+		return node;
+	}
+	
+	public override void NodeGUI () 
+	{
+		GUILayout.BeginHorizontal ();
+		GUILayout.Label (new GUIContent ("Value : " + (assigned? value.ToString () : ""), "The input value to display"));
+		if (Event.current.type == EventType.Repaint) 
+			Inputs [0].SetRect (GUILayoutUtility.GetLastRect ());
+		GUILayout.EndHorizontal ();
+	}
+	
+	public override bool Calculate () 
+	{
+		if (!allInputsReady ()) 
+		{
+			value = 0;
+			assigned = false;
+			return false;
+		}
 
-        node.Init();
-        return node;
-    }
+		value = (float)Inputs [0].connection.value;
+		assigned = true;
 
-    public override void DrawNode() {
-        GUILayout.BeginHorizontal();
-        GUILayout.Label(new GUIContent("Value : " + (assigned ? value.ToString() : ""), "The input value to display"));
-        if (Event.current.type == EventType.Repaint)
-            Inputs[0].SetRect(GUILayoutUtility.GetLastRect());
-        GUILayout.EndHorizontal();
-    }
-
-    public override bool Calculate() {
-        if (!allInputsReady()) {
-            value = 0;
-            assigned = false;
-            return false;
-        }
-
-        value = (float) Inputs[0].connection.value;
-        assigned = true;
-
-        return true;
-    }
+		return true;
+	}
 }
