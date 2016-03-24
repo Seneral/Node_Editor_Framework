d3471434a37f636829e330deefd18bbd05c396f9
rudinesurya <rudinesurya@gmail.com>
2016-03-24 18:54:20 +0800

    asd

---
+++ Node_Editor/Framework/Extension/RectExt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class RectExt
+{
+    /// <summary>
+    /// Returns true if rect2 is fully inside rect1
+    /// </summary>
+    /// <param name="rect1"></param>
+    /// <param name="rect2"></param>
+    /// <returns></returns>
+    public static bool Contains(this Rect rect1, Rect rect2)
+    {
+        return rect1.Contains(rect2.min) && rect1.Contains(rect2.max);
+    }
+}
