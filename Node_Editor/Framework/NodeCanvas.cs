a7d4b419f4f9a2e6dea7d7b70c160fa0880553e7
Gil Ferraz <gil.ferraz.gomes@outlook.com>
2017-01-18 23:21:11 +0000

    Improved code documentation. Also converted a foreach loop to a for to avoid generating garbage.

--- Node_Editor/Framework/NodeCanvas.cs
+++ Node_Editor/Framework/NodeCanvas.cs
@@ -1,198 +1,250 @@
-﻿using UnityEngine;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using NodeEditorFramework.Standard;
-
-namespace NodeEditorFramework 
+﻿namespace NodeEditorFramework
 {
-	/// <summary>
-	/// NodeCanvas base class
-	/// </summary>
-	public class NodeCanvas : ScriptableObject 
-	{
-		public virtual string canvasName { get { return "DEFAULT"; } }
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using NodeEditorFramework.Standard;
+    using UnityEngine;
 
-		public NodeCanvasTraversal Traversal;
+    /// <summary>
+    /// The Node Canvas base class.
+    /// </summary>
+    /// <remarks>
+    /// All canvases should derive from this class.
+    /// </remarks>
+    public class NodeCanvas : ScriptableObject
+    {
+        //TODO: As per C# design guidelines, there should be no exposed (public/protected) fields.
+        //TODO: To avoid this, they should be converted to proprieties with backing fields 
+        //TODO: with the [SerializeField] propriety to allow access to Unity's ScriptableObject.
 
-		public NodeEditorState[] editorStates = new NodeEditorState[0];
+        public virtual string canvasName { get { return "DEFAULT"; } }
 
-		public string saveName;
-		public string savePath;
+        public NodeCanvasTraversal Traversal;
 
-		public bool livesInScene = false;
+        public NodeEditorState[] editorStates = new NodeEditorState[0];
 
-		public List<Node> nodes = new List<Node> ();
-		public List<NodeGroup> groups = new List<NodeGroup> ();
+        public string saveName;
+        public string savePath;
 
-		#region Constructors
+        public bool livesInScene;
 
-		public static T CreateCanvas<T> () where T : NodeCanvas
-		{
-			if (typeof(T) == typeof(NodeCanvas))
-				throw new Exception ("Cannot create canvas of type 'NodeCanvas' as that is only the base class. Please specify a valid subclass!");
-			T canvas = ScriptableObject.CreateInstance<T>();
-			canvas.name = canvas.saveName = "New " + canvas.canvasName;
+        public List<Node> nodes = new List<Node>();
+        public List<NodeGroup> groups = new List<NodeGroup>();
 
-			NodeEditor.BeginEditingCanvas (canvas);
-			canvas.OnCreate ();
-			NodeEditor.EndEditingCanvas ();
-			return canvas;
-		}
+        #region Constructors
 
-		public static NodeCanvas CreateCanvas (Type canvasType)
-		{
-			NodeCanvas canvas;
-			if (canvasType != null && canvasType.IsSubclassOf (typeof(NodeCanvas)))
-				canvas = ScriptableObject.CreateInstance (canvasType) as NodeCanvas;
-			else
-				canvas = ScriptableObject.CreateInstance<CalculationCanvasType>();
-			canvas.name = canvas.saveName = "New " + canvas.canvasName;
+        /// <summary>
+        /// Initializes the <see cref="NodeCanvas"/>.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="NodeCanvas"/> subclass's type.</typeparam>
+        /// <returns>Returns the initialized <see cref="NodeCanvas"/>.</returns>
+        public static T CreateCanvas<T>() where T : NodeCanvas
+        {
+            if (typeof(T) == typeof(NodeCanvas))
+            {
+                throw new Exception("Cannot create canvas of type 'NodeCanvas' as that is only the base class. Please specify a valid subclass!");
+            }
 
-			NodeEditor.BeginEditingCanvas (canvas);
-			canvas.OnCreate ();
-			NodeEditor.EndEditingCanvas ();
-			return canvas;
-		}
+            T canvas = CreateInstance<T>();
+            canvas.name = canvas.saveName = "New " + canvas.canvasName;
 
-		#endregion
+            NodeEditor.BeginEditingCanvas(canvas);
+            canvas.OnCreate();
+            NodeEditor.EndEditingCanvas();
 
-		#region Callbacks
+            return canvas;
+        }
 
-		protected virtual void OnCreate () {}
+        /// <summary>
+        /// Initializes the <see cref="NodeCanvas"/>.
+        /// </summary>
+        /// <param name="canvasType">The <see cref="NodeCanvas"/> subclass's type.</param>
+        /// <returns>Returns the initialized <see cref="NodeCanvas"/>.</returns>
+        public static NodeCanvas CreateCanvas(Type canvasType)
+        {
+            NodeCanvas canvas;
 
-		protected virtual void OnValidate () { }
+            if (canvasType != null && canvasType.IsSubclassOf(typeof(NodeCanvas)))
+            {
+                canvas = CreateInstance(canvasType) as NodeCanvas;
+            }
+            else
+            {
+                canvas = CreateInstance<CalculationCanvasType>();
+            }
 
-		public virtual void OnBeforeSavingCanvas () { }
+            canvas.name = canvas.saveName = "New " + canvas.canvasName;
 
-		public virtual bool CanAddNode (string nodeID) { return true; }
+            NodeEditor.BeginEditingCanvas(canvas);
+            canvas.OnCreate();
+            NodeEditor.EndEditingCanvas();
 
-		#endregion
+            return canvas;
+        }
 
-		#region Traversal
+        #endregion
 
-		public void TraverseAll ()
-		{
-			if (Traversal != null)
-				Traversal.TraverseAll ();
-		}
+        #region Callbacks
 
-		public void OnNodeChange (Node node)
-		{
-			if (Traversal != null && node != null)
-				Traversal.OnChange (node);
-		}
+        protected virtual void OnCreate() {}
 
-		#endregion
+        protected virtual void OnValidate() {}
 
-		#region Methods
+        public virtual void OnBeforeSavingCanvas() {}
 
-		/// <summary>
-		/// Validates this canvas, checking for any broken nodes or references and cleans them.
-		/// </summary>
-		public void Validate (bool deepValidate = false) 
-		{
-			if (nodes == null)
-			{
-				Debug.LogWarning ("NodeCanvas '" + name + "' nodes were erased and set to null! Automatically fixed!");
-				nodes = new List<Node> ();
-			}
-			if (deepValidate)
-			{
-				for (int groupCnt = 0; groupCnt < groups.Count; groupCnt++) 
-				{
-					NodeGroup group = groups[groupCnt];
-					if (group == null)
-					{
-						Debug.LogWarning ("NodeCanvas '" + name + "' contained broken (null) group! Automatically fixed!");
-						groups.RemoveAt (groupCnt);
-						groupCnt--;
-						continue;
-					}
-				}
-				for (int nodeCnt = 0; nodeCnt < nodes.Count; nodeCnt++) 
-				{
-					Node node = nodes[nodeCnt];
-					if (node == null)
-					{
-						Debug.LogWarning ("NodeCanvas '" + saveName + "' (" + name + ") contained broken (null) node! Automatically fixed!");
-						nodes.RemoveAt (nodeCnt);
-						nodeCnt--;
-						continue;
-					}
-					for (int knobCnt = 0; knobCnt < node.nodeKnobs.Count; knobCnt++) 
-					{
-						NodeKnob nodeKnob = node.nodeKnobs[knobCnt];
-						if (nodeKnob == null)
-						{
-							Debug.LogWarning ("NodeCanvas '" + name + "' Node '" + node.name + "' contained broken (null) NodeKnobs! Automatically fixed!");
-							nodes.RemoveAt (nodeCnt);
-							nodeCnt--;
-							break;
-//							node.nodeKnobs.RemoveAt (knobCnt);
-//							knobCnt--;
-//							continue;
-						}
+        public virtual bool CanAddNode(string nodeID)
+        {
+            return true;
+        }
 
-						if (nodeKnob is NodeInput)
-						{
-							NodeInput input = nodeKnob as NodeInput;
-							if (input.connection != null && input.connection.body == null)
-							{ // References broken node; Clear connection
-								input.connection = null;
-							}
-							//						for (int conCnt = 0; conCnt < (nodeKnob as NodeInput).connection.Count; conCnt++)
-						}
-						else if (nodeKnob is NodeOutput)
-						{
-							NodeOutput output = nodeKnob as NodeOutput;
-							for (int conCnt = 0; conCnt < output.connections.Count; conCnt++) 
-							{
-								NodeInput con = output.connections[conCnt];
-								if (con == null || con.body == null)
-								{ // Broken connection; Clear connection
-									output.connections.RemoveAt (conCnt);
-									conCnt--;
-								}
-							}
-						}
-					}
-				}
-				if (editorStates == null)
-				{
-					Debug.LogWarning ("NodeCanvas '" + name + "' editorStates were erased! Automatically fixed!");
-					editorStates = new NodeEditorState[0];
-				}
-				editorStates = editorStates.Where ((NodeEditorState state) => state != null).ToArray ();
-				foreach (NodeEditorState state in editorStates)
-				{
-					if (!nodes.Contains (state.selectedNode))
-						state.selectedNode = null;
-				}
-			}
-			OnValidate ();
-		}
+        #endregion
 
-		public void UpdateSource (string path) 
-		{
-			string newName;
-			if (path.StartsWith ("SCENE/"))
-			{
-				newName = path.Substring (6);
-			}
-			else
-			{
-				int nameStart = path.LastIndexOf ('/')+1;
-				newName = path.Substring (nameStart, path.Length-nameStart-6);
-			}
-			if (newName != "LastSession")
-			{
-				savePath = path;
-				saveName = newName;
-			}
-		}
+        #region Traversal
 
-		#endregion
-	}
+        public void TraverseAll()
+        {
+            if (Traversal != null)
+                Traversal.TraverseAll();
+        }
+
+        public void OnNodeChange(Node node)
+        {
+            if (Traversal != null && node != null)
+                Traversal.OnChange(node);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Validates the <see cref="NodeCanvas"/> by checking for any broken references.
+        /// If there are any, cleans them.
+        /// </summary>
+        /// <param name="deepValidate"></param>
+        public void Validate(bool deepValidate = false)
+        {
+            if (nodes == null)
+            {
+                Debug.LogWarning("The NodeCanvas '" + name + "' nodes were erased and set to null! Automatically fixed!");
+                nodes = new List<Node>();
+            }
+
+            if (deepValidate)
+            {
+                // Checks for broken references in each group.
+                for (int groupCnt = 0; groupCnt < groups.Count; groupCnt++)
+                {
+                    NodeGroup group = groups[groupCnt];
+
+                    if (group == null)
+                    {
+                        Debug.LogWarning("The NodeCanvas '" + name + "' contained broken (null) group! Automatically fixed!");
+                        groups.RemoveAt(groupCnt);
+                        groupCnt--;
+                    }
+                }
+
+                // Checks for broken references in each node.
+                for (int nodeCnt = 0; nodeCnt < nodes.Count; nodeCnt++)
+                {
+                    Node node = nodes[nodeCnt];
+
+                    if (node == null)
+                    {
+                        Debug.LogWarning("The NodeCanvas '" + saveName + "' (" + name + ") contained broken (null) node! Automatically fixed!");
+                        nodes.RemoveAt(nodeCnt);
+                        nodeCnt--;
+                        continue;
+                    }
+
+                    // Checks for broken references in each knob of the node.
+                    for (int knobCnt = 0; knobCnt < node.nodeKnobs.Count; knobCnt++)
+                    {
+                        NodeKnob nodeKnob = node.nodeKnobs[knobCnt];
+
+                        if (nodeKnob == null)
+                        {
+                            Debug.LogWarning("The NodeCanvas '" + name + "' Node '" + node.name + "' contained broken (null) NodeKnobs! Automatically fixed!");
+                            nodes.RemoveAt(nodeCnt);
+                            nodeCnt--;
+                            break;
+
+                            //node.nodeKnobs.RemoveAt(knobCnt);
+                            //knobCnt--;
+                            //continue;
+                        }
+
+                        if (nodeKnob is NodeInput)
+                        {
+                            NodeInput input = nodeKnob as NodeInput;
+                            if (input.connection != null && input.connection.body == null)
+                            {
+                                // References broken node; Clear connection
+                                input.connection = null;
+                            }
+                            //for (int conCnt = 0; conCnt < (nodeKnob as NodeInput).connection.Count; conCnt++)
+                        }
+                        else if (nodeKnob is NodeOutput)
+                        {
+                            NodeOutput output = nodeKnob as NodeOutput;
+                            for (int conCnt = 0; conCnt < output.connections.Count; conCnt++)
+                            {
+                                NodeInput con = output.connections[conCnt];
+                                if (con == null || con.body == null)
+                                {
+                                    // Broken connection; Clear connection
+                                    output.connections.RemoveAt(conCnt);
+                                    conCnt--;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // Checks for broken references in the editor states.
+                if (editorStates == null)
+                {
+                    Debug.LogWarning("NodeCanvas '" + name + "' editorStates were erased! Automatically fixed!");
+                    editorStates = new NodeEditorState[0];
+                }
+
+                editorStates = editorStates.Where(state => state != null).ToArray();
+
+                for (int i = 0; i < editorStates.Length; i++)
+                {
+                    NodeEditorState state = editorStates[i];
+                    if (!nodes.Contains(state.selectedNode))
+                    {
+                        state.selectedNode = null;
+                    }
+                }
+            }
+            OnValidate();
+        }
+
+        public void UpdateSource(string path)
+        {
+            string newName;
+
+            if (path.StartsWith("SCENE/"))
+            {
+                newName = path.Substring(6);
+            }
+            else
+            {
+                int nameStart = path.LastIndexOf('/') + 1;
+                newName = path.Substring(nameStart, path.Length - nameStart - 6);
+            }
+
+            if (newName != "LastSession")
+            {
+                savePath = path;
+                saveName = newName;
+            }
+        }
+
+        #endregion
+    }
 }
