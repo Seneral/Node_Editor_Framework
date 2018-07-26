3ce53232cd3209d8944c212c508a945cb78b0e6d
Jannie Pieters <jannie.pieters@gmail.com>
2018-07-26 14:27:12 +0200

    Fixed isInput and isOutput returning true bug

--- Node_Editor/Framework/Core/Node.cs
+++ Node_Editor/Framework/Core/Node.cs
@@ -378,7 +378,7 @@ namespace NodeEditorFramework
 		public bool isInput()
 		{
 			for (int i = 0; i < inputPorts.Count; i++)
-				if (!inputPorts[i].connected())
+				if (inputPorts[i].connected())
 					return false;
 			return true;
 		}
@@ -389,7 +389,7 @@ namespace NodeEditorFramework
 		public bool isOutput()
 		{
 			for (int i = 0; i < outputPorts.Count; i++)
-				if (!outputPorts[i].connected())
+				if (outputPorts[i].connected())
 					return false;
 			return true;
 		}
